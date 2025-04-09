5c7ca4d7e2be0e02b7d136e79215855cd0197798
grainfree <jernej.furman@gmail.com>
2025-04-09 13:40:05 +0200

    Dodana uporabniška stran s značkami in povezavami

---
+++ DoggyDrop/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DoggyDrop.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public int TotalBins { get; set; }
+        public List<string> Badges { get; set; } = new();
+    }
+}
