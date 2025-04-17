42c62f4a10bc81e2fb62bd6e49baea7dec79a7ab
grainfree <jernej.furman@gmail.com>
2025-04-17 15:50:16 +0200

    ✅ Dodan TrashBinViewModel z imenom, GPS in sliko

---
+++ DoggyDrop/ViewModels/TrashBinViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoggyDrop.ViewModels
+{
+    public class TrashBinViewModel
+    {
+        [Required(ErrorMessage = "Ime koša je obvezno.")]
+        [Display(Name = "Ime")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Display(Name = "Zemljepisna širina")]
+        public double Latitude { get; set; }
+
+        [Required]
+        [Display(Name = "Zemljepisna dolžina")]
+        public double Longitude { get; set; }
+
+        [Display(Name = "Fotografija")]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
