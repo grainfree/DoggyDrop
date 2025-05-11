d5baa4393f196af7f807ec8ef343414585935306
grainfree <jernej.furman@gmail.com>
2025-05-11 19:30:42 +0200

    🔔 Dodano pošiljanje e-mail obvestil ob registraciji in predlogih košev

--- DoggyDrop/Services/EmailSender.cs
+++ DoggyDrop/Services/EmailSender.cs
@@ -3,7 +3,6 @@ using MailKit.Security;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using MimeKit;
-using System.Net.Mail;
 
 namespace DoggyDrop.Services
 {
@@ -19,26 +18,18 @@ namespace DoggyDrop.Services
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("DoggyDrop", _emailSettings.Sender));
-            message.To.Add(new MailboxAddress(email));
+            message.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.SenderEmail));
+            message.To.Add(MailboxAddress.Parse(email));
             message.Subject = subject;
 
             var bodyBuilder = new BodyBuilder { HtmlBody = htmlMessage };
             message.Body = bodyBuilder.ToMessageBody();
 
             using var client = new SmtpClient();
-            await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, SecureSocketOptions.StartTls);
-            await client.AuthenticateAsync(_emailSettings.Sender, _emailSettings.Password);
+            await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.SmtpPort, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_emailSettings.SmtpUser, _emailSettings.SmtpPass);
             await client.SendAsync(message);
             await client.DisconnectAsync(true);
         }
-    }
-
-    public class EmailSettings
-    {
-        public string SmtpServer { get; set; }
-        public int Port { get; set; }
-        public string Sender { get; set; }
-        public string Password { get; set; }
     }
 }
